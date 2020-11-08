db55dd6213253d8caa904e8a7942123ce27ad4ed
Wu Xueheng <xhwu@thoughtworks.com>
2020-11-08 20:38:44 +0800

    fix: fix dependency rename in BackEndApiTemplateTest.csproj

--- rename.sh
+++ rename.sh
@@ -5,7 +5,7 @@ sed -i "" "s/BackEndApiTemplateTest/$1Test/" BackEndApiTemplateTest/BackEndApiTe
 sed -i "" "s/backendapitemplate/$1/" BackEndApiTemplate/BackEndApiTemplate.csproj
 sed -i "" "s/BackEndApiTemplateTest/$1Test/" BackEndApiTemplateTest/UnitTest1.cs
 sed -i "" "s/BackEndApiTemplate/$1/" BackEndApiTemplate/Startup.cs  BackEndApiTemplate/Program.cs BackEndApiTemplate/Controllers/HelloController.cs
-sed -i "" "s/BackEndApiTemplateTest/$1/g" BackEndApiTemplateTest/BackEndApiTemplateTest.csproj
+sed -i "" "s/BackEndApiTemplate/$1/g" BackEndApiTemplateTest/BackEndApiTemplateTest.csproj
 sed -i "" "s/BackEndApiTemplateTest/$1Test/g" BackEndApiTemplate.sln
 sed -i "" "s/BackEndApiTemplate/$1/g" BackEndApiTemplate.sln
 
