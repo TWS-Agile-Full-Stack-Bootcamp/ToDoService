36985b1c50002ef356d41b08da081c2e7393148c
Chen Xu <xuchen@thoughtworks.com>
2020-11-08 13:07:23 +0800

    add rename.sh

---
+++ rename.sh
@@ -0,0 +1,22 @@
+#! zsh
+
+sed -i "" -e "s/backendapitemplatetest/$1Test/" -e "s/backendapitemplate/$1/" .gitignore
+sed -i "" "s/BackEndApiTemplate_test/$1Test/" backendapitemplatetest/backendapitemplatetest.csproj
+sed -i "" "s/BackEndApiTemplate/$1/" backendapitemplate/backendapitemplate.csproj
+sed -i "" "s/BackEndApiTemplate_test/$1Test/" backendapitemplatetest/UnitTest1.cs
+sed -i "" "s/BackEndApiTemplate/$1/" backendapitemplate/Class1.cs backendapitemplatetest/UnitTest1.cs
+sed -i "" "s/backendapitemplate/$1/g" backendapitemplatetest/backendapitemplatetest.csproj
+sed -i "" "s/backendapitemplatetest/$1Test/g" backendapitemplate.sln
+sed -i "" "s/backendapitemplate/$1/g" backendapitemplate.sln
+
+mv backendapitemplatetest/backendapitemplatetest.csproj backendapitemplatetest/$1Test.csproj
+mv backendapitemplate/backendapitemplate.csproj backendapitemplate/$1.csproj
+mv backendapitemplatetest $1Test
+mv backendapitemplate $1
+mv backendapitemplate.sln $1.sln
+mv .vs/backendapitemplate .vs/$1
+
+rm README.md
+rm rename.sh
+git add .
+git commit -m "chore: rename project to $1"
