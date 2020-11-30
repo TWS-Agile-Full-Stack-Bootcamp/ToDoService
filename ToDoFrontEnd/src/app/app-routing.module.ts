0a814d1340cde4b490b932fdeca92f2e9842c0f0
ChenXu <xuchen@thoughtworks.com>
2020-11-30 21:25:51 +0800

    feat: add routing

--- ToDoFrontEnd/src/app/app-routing.module.ts
+++ ToDoFrontEnd/src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ListTodoitemComponent } from './list-todoitem/list-todoitem.component';
+import { TodoitemDetailComponent } from './todoitem-detail/todoitem-detail.component';
 import { CreateTodoitemComponent } from './create-todoitem/create-todoitem.component';
+import { UpdateTodoItemComponent } from './update-todo-item/update-todo-item.component';
 
 const routes: Routes = [
   { path: "", redirectTo: '/index', pathMatch: 'full' },
   { path: "index", component: ListTodoitemComponent },
-  { path: "create", component: CreateTodoitemComponent }
+  { path: "create", component: CreateTodoitemComponent },
+  { path: "detail", component: TodoitemDetailComponent },
+  { path: "edit", component: UpdateTodoItemComponent },
 ];
 
 @NgModule({
